629e3052b2c534eeef6f19f40c811520e11cb00d
Yuto Watanabe <yuto.w51942@gmail.com>
2022-07-12 20:55:54 +0900

    mock handler追加

---
+++ mock_handler/mock_handler.go
@@ -0,0 +1,49 @@
+package mockhandler
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+type MockHandler struct {
+	W *httptest.ResponseRecorder
+	R *http.Request
+}
+
+// handlerのwとrをmockする
+func NewMock(body string, method string, path string) *MockHandler {
+	b := strings.NewReader(body)
+	return NewMockByte(b, method, path)
+}
+
+func NewMockByte(body io.Reader, method string, path string) *MockHandler {
+	r := httptest.NewRequest(method, path, body)
+
+	w := httptest.NewRecorder()
+
+	return &MockHandler{
+		W: w,
+		R: r,
+	}
+}
+
+func (c *MockHandler) MockHandler(hand func(w http.ResponseWriter, r *http.Request)) {
+	hand(c.W, c.R)
+}
+
+func (c *MockHandler) Ok(t *testing.T) {
+	c.Status(t, http.StatusOK)
+}
+
+func (c *MockHandler) Status(t *testing.T, status int) {
+	require.Equal(t, c.W.Code, status)
+}
+
+func (c *MockHandler) EqBody(t *testing.T, body string) {
+	require.Equal(t, c.W.Body.String(), body)
+}
---
+++ mock_handler/mock_server.go
@@ -0,0 +1,112 @@
+package mockhandler
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strings"
+	"testing"
+
+	"github.com/cateiru/go-http-easy-test/contents"
+	"github.com/stretchr/testify/require"
+)
+
+var client = new(http.Client)
+
+type MockServer struct {
+	Server *httptest.Server
+}
+
+// モックサーバーを起動する
+func NewMockServer(handler *http.ServeMux) *MockServer {
+	server := httptest.NewServer(handler)
+
+	return &MockServer{
+		Server: server,
+	}
+}
+
+// TLSでモックサーバーを起動する
+func NewMockTLSServer(handler *http.ServeMux) *MockServer {
+	server := httptest.NewTLSServer(handler)
+
+	return &MockServer{
+		Server: server,
+	}
+}
+
+// サーバーをclose
+func (c *MockServer) Close() {
+	c.Server.Close()
+}
+
+// モックサーバー用のURLに変換する
+func (c *MockServer) URL(path string) string {
+	return c.Server.URL + path
+}
+
+// Getメソッドで取得
+func (c *MockServer) Get(t *testing.T, path string) *http.Response {
+	resp, err := http.Get(c.URL(path))
+	require.NoError(t, err)
+
+	return resp
+}
+
+// Getメソッドで取得し、レスポンスステータスが200かどうかを確認する
+func (c *MockServer) GetOK(t *testing.T, path string) *http.Response {
+	resp := c.Get(t, path)
+
+	require.Equal(t, resp.StatusCode, http.StatusOK)
+
+	return resp
+}
+
+func (c *MockServer) Post(t *testing.T, path string, contentType string, body io.Reader) *http.Response {
+	resp, err := http.Post(c.URL(path), contentType, body)
+	require.NoError(t, err)
+
+	return resp
+}
+
+// application/x-www-form-urlencoded
+func (c *MockServer) PostForm(t *testing.T, path string, value url.Values) *http.Response {
+	resp, err := http.PostForm(c.URL(path), value)
+	require.NoError(t, err)
+
+	return resp
+}
+
+func (c *MockServer) PostString(t *testing.T, path string, contentType string, body string) *http.Response {
+	r := strings.NewReader(body)
+	resp := c.Post(t, path, contentType, r)
+
+	return resp
+}
+
+// POST multipart/form-data
+func (c *MockServer) PostFormData(t *testing.T, path string, form *contents.Multipart) *http.Response {
+	return c.FormData(t, path, http.MethodPost, form)
+}
+
+// multipart/form-data
+func (c *MockServer) FormData(t *testing.T, path string, method string, form *contents.Multipart) *http.Response {
+	body := form.Export()
+
+	return c.Do(t, path, method, body, func(r *http.Request) {
+		r.Header.Add("Content-Type", form.ContentType())
+	})
+}
+
+func (c *MockServer) Do(t *testing.T, path string, method string, body io.Reader, before func(r *http.Request)) *http.Response {
+	r, err := http.NewRequest(method, c.URL(path), body)
+	require.NoError(t, err)
+
+	before(r)
+
+	resp, err := client.Do(r)
+	require.NoError(t, err)
+
+	return resp
+}
